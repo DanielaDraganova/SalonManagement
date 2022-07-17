4792b88ac99f283fdabd18dc3e3d7044a48b2421
DanielaDraganova <daniela.st.draganova@gmail.com>
2022-07-17 22:09:17 +0300

    Add login and registration with firebase

--- src/Components/AppRouter.js
+++ src/Components/AppRouter.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 function AppRouter() {
   return (
@@ -7,6 +9,9 @@ function AppRouter() {
       <Routes>
         <Route path="/home" element={<div>Home page</div>}></Route>
         <Route path="/catalog" element={<div>Catalog page</div>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </BrowserRouter>
   );
