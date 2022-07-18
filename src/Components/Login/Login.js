ee5231159f8a86557bb4fe6c6397acd1f3341283
DanielaDraganova <daniela.st.draganova@gmail.com>
2022-07-18 13:08:02 +0300

    Change Register and Login css to modules

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,7 +6,7 @@ import {
   signInWithGoogle,
 } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import "./Login.css";
+import styles from "../Login/Login.module.css";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -20,29 +20,32 @@ function Login() {
     if (user) navigate("/home");
   }, [user, loading]);
   return (
-    <div className="login">
-      <div className="login__container">
+    <div className={styles.login}>
+      <div className={styles["login__container"]}>
         <input
           type="text"
-          className="login__textBox"
+          className={styles["login__textBox"]}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="E-mail Address"
         />
         <input
           type="password"
-          className="login__textBox"
+          className={styles["login__textBox"]}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
         <button
-          className="login__btn"
+          className={styles["login__btn"]}
           onClick={() => logInWithEmailAndPassword(email, password)}
         >
           Login
         </button>
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
+        <button
+          className={styles["login__btn login__google"]}
+          onClick={signInWithGoogle}
+        >
           Login with Google
         </button>
         <div>
